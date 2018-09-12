25b15febd80b766d2bfdcf72c5375d6433f00bf6
584807419 <584807419@qq.com>
2018-09-12 17:48:47 +0800

    部署后的可用版本

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # ZhiHu
-简介：学习python的过程中用Flask做的知乎  
-网址http://154.8.219.34:82  
-2018.9.12迁移到京东云
+用Flask做的知乎，功能不断完善中
 
 ![image](https://github.com/584807419/ZhiHu/blob/dev/images/index.png)
--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from flask import render_template, redirect, request, url_for, flash, abort,current_app,make_response
 from flask_login import login_user,logout_user,login_required,current_user
 from ..models import User,Permission,Post,Role,Comment,Answer
