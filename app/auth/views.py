b36651cfe2636a93a178759b4e92b1b500156f42
584807419 <584807419@qq.com>
2018-12-22 13:23:47 +0800

    fix utf-8

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,13 @@
-#coding:utf-8
-#coding:utf-8
+# coding:utf-8
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user,logout_user,login_required,current_user
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm,RegisteationForm,ChangePasswordForm,PasswordResetForm,PasswordResetRequestForm,ChangeEmailForm
+from .forms import LoginForm, RegisteationForm, ChangePasswordForm, PasswordResetForm, PasswordResetRequestForm, \
+    ChangeEmailForm
 from .. import db
 from ..email import send_email
+
 
 # before_request 钩子只能应用到属于蓝本的请求上。若想在蓝本中使用针对程序全局请求的钩子，必须使用 before_app_request 修饰器
 @auth.before_app_request
@@ -19,14 +20,16 @@ def before_request():
                 and request.endpoint != 'static':
             return redirect(url_for('auth.unconfirmed'))
 
+
 @auth.route('/unconfirmed')
 def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
 
-#用户登录
-#提交登陆表单LoginForm后，如果数据能被所有验证函数接受，那么 validate_on_submit() 方法的返回值为 True 并执行登陆login_user，否则返回 False,让用户重试
+
+# 用户登录
+# 提交登陆表单LoginForm后，如果数据能被所有验证函数接受，那么 validate_on_submit() 方法的返回值为 True 并执行登陆login_user，否则返回 False,让用户重试
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -37,11 +40,12 @@ def login():
             flash('登录成功!')
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('用户名或者密码错误,请重试!')
-    #return render_template('auth/login.html',form=form)使用{{ wtf.quick_form(form) }}渲染表单方式登陆,先留着备用
-    return render_template('auth/login1.html', form=form)#改用知乎样式,并且使用手写form表单的形式登陆
+    # return render_template('auth/login.html',form=form)使用{{ wtf.quick_form(form) }}渲染表单方式登陆,先留着备用
+    return render_template('auth/login1.html', form=form)  # 改用知乎样式,并且使用手写form表单的形式登陆
 
-#用户退出登录
-#Flask-Login 提供了一个 login_required 修饰器,如果未认证的用户访问这个路由，Flask-Login 会拦截请求，把用户发往登录页面。
+
+# 用户退出登录
+# Flask-Login 提供了一个 login_required 修饰器,如果未认证的用户访问这个路由，Flask-Login 会拦截请求，把用户发往登录页面。
 @auth.route('/logout')
 @login_required
 def logout():
@@ -49,55 +53,58 @@ def logout():
     flash('您已经退出登录')
     return redirect(url_for('main.index'))
 
-#新用户注册
+
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    新用户注册
+    注意，即便通过配置，程序已经可以在请求末尾自动提交数据库变化，这里也要添加db.session.commit() 调用。
+    问题在于，提交数据库之后才能赋予新用户 id 值，而确认令牌需要用到 id ，所以不能延后提交
+    :return:
+    """
     form = RegisteationForm()
     if form.validate_on_submit():
-        print("come in")
-        user = User(
-            email=form.email.data,
-            username=form.username.data,
-            password=form.password.data
-        )
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
-        #注意，即便通过配置，程序已经可以在请求末尾自动提交数据库变化，这里也要添加db.session.commit() 调用。问题在于，提交数据库之后才能赋予新用户 id 值，而确认令牌需要用到 id ，所以不能延后提交。
         db.session.commit()
         token = user.generate_confirmtion_token()
-        send_email(user.email,u'请确认您的账户邮箱','auth/email/confirm',user=user,token=token)
-        flash('一封确认邮件已经发送到您的邮箱')
-        flash('账号注册成功!')
+        login_user(user)
+        send_email(user.email, u'请确认您的账户邮箱', 'auth/email/confirm', user=user, token=token)
+        flash(u'注册成功,一封确认邮件已经发送到您的邮箱')
         return redirect(url_for('main.index'))
-    # return render_template('auth/register.html', form=form)
-    return render_template('auth/register1.html', form=form)#知乎注册样式
+    return render_template('auth/register1.html', form=form)
 
-#重发激活邮件
+
+# 重发激活邮件
 @auth.route('/resend_confirmation')
 @login_required
 def resend_confirmation():
     token = current_user.generate_confirmtion_token()
-    #第二个参数是指定templates里面的模版
+    # 第二个参数是指定templates里面的模版
     send_email(current_user.email, u'请确认您的账户邮箱', 'auth/email/confirm', user=current_user, token=token)
     flash('一封确认邮件已经发送到您的邮箱')
     return redirect(url_for('main.index'))
 
-#确认用户的账户
+
+# 确认用户的账户
 @auth.route('/confirm/<token>')
 @login_required
 def confirm(token):
-    #除了检验令牌， confirm() 方法还检查令牌中的 id 是否和存储在 current_user 中的已登录用户匹配。如此一来，即使恶意用户知道如何生成签名令牌，也无法确认别人的账户。
-    #由于令牌确认完全在User 模型中完成，所以视图函数只需调用confirm()方法即可，然后 再根据确认结果显示不同的Flash消息。确认成功后，User模型中confirmed属性的值会被修改并添加到会话中，请求处理完后，这两个操作被提交到数据库。
-    #通过current_user这个User()类的一个实例来调用类中的方法非常的方便
+    # 除了检验令牌， confirm() 方法还检查令牌中的 id 是否和存储在 current_user 中的已登录用户匹配。如此一来，即使恶意用户知道如何生成签名令牌，也无法确认别人的账户。
+    # 由于令牌确认完全在User 模型中完成，所以视图函数只需调用confirm()方法即可，然后 再根据确认结果显示不同的Flash消息。确认成功后，User模型中confirmed属性的值会被修改并添加到会话中，请求处理完后，这两个操作被提交到数据库。
+    # 通过current_user这个User()类的一个实例来调用类中的方法非常的方便
     if current_user.confirmed:
+        flash(u'您的账户已确认成功')
         return redirect(url_for('main.index'))
     if current_user.confirm(token):
-        flash('您的账户已确认成功 !')
+        flash(u'您的账户已确认成功')
     else:
-        flash('确认失败,请重新发送确认邮件再试')
+        flash(u'确认失败,请重新发送确认邮件再试')
     return redirect(url_for('main.index'))
 
-#修改密码
-@auth.route('/change-passord',methods=['GET','POST'])
+
+# 修改密码
+@auth.route('/change-passord', methods=['GET', 'POST'])
 @login_required
 def change_password():
     form = ChangePasswordForm()
@@ -111,8 +118,9 @@ def change_password():
             flash(u'密码无效')
     return render_template("auth/change_password.html", form=form)
 
-#重置密码的请求
-@auth.route('/reset',methods=['GET','POST'])
+
+# 重置密码的请求
+@auth.route('/reset', methods=['GET', 'POST'])
 def password_reset_request():
     if not current_user.is_anonymous:
         return redirect(url_for('main.index'))
@@ -121,13 +129,14 @@ def password_reset_request():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             token = user.generate_reset_token()
-            send_email(user.email,u'重置你的密码','auth/email/reset_passord',user=user,token=token,next=request.args.get('next'))
+            send_email(user.email, u'重置你的密码', 'auth/email/reset_passord', user=user, token=token,
+                       next=request.args.get('next'))
         flash(u'一封重置密码邮件已经发给你了呦!')
     return render_template('auth/reset_password.html', form=form)
 
 
-#点了邮件中的链接后重置密码实际操作过程,验证token
-@auth.route('/reset/<token>',methods=['GET','POST'])
+# 点了邮件中的链接后重置密码实际操作过程,验证token
+@auth.route('/reset/<token>', methods=['GET', 'POST'])
 def password_reset(token):
     if not current_user.is_anonymous:
         return redirect(url_for('main.index'))
@@ -141,10 +150,11 @@ def password_reset(token):
             return redirect(url_for('auth.login'))
         else:
             return redirect(url_for('main.index'))
-    return render_template('auth/reset_password.html',form=form)
+    return render_template('auth/reset_password.html', form=form)
 
-#发送修改电子邮件的地址的请求
-@auth.route('/change-email',methods=['GET','POST'])
+
+# 发送修改电子邮件的地址的请求
+@auth.route('/change-email', methods=['GET', 'POST'])
 @login_required
 def change_email_request():
     form = ChangeEmailForm()
@@ -152,14 +162,15 @@ def change_email_request():
         if current_user.verify_password(form.password.data):
             new_email = form.email.data
             token = current_user.generate_email_change_token(new_email)
-            send_email(new_email,u'确认你的新邮箱地址','auth/email/change_email',user=current_user,token=token)
+            send_email(new_email, u'确认你的新邮箱地址', 'auth/email/change_email', user=current_user, token=token)
             flash(u'一封修改邮箱的确认邮件已经发给你了呦!')
             return redirect(url_for('main.index'))
         else:
             flash(u'无效的邮箱或者密码')
-    return render_template("auth/change_email.html",form=form)
+    return render_template("auth/change_email.html", form=form)
 
-#修改邮箱的实际操作过程
+
+# 修改邮箱的实际操作过程
 @auth.route('/change_email/<token>')
 @login_required
 def change_email(token):
--- app/main/views.py
+++ app/main/views.py
@@ -1,115 +1,103 @@
-#coding:utf-8
-#coding:utf-8
-from flask import render_template, redirect, request, url_for, flash, abort,current_app,make_response
-from flask_login import login_user,logout_user,login_required,current_user
-from ..models import User,Permission,Post,Role,Comment,Answer
-from .forms import PostForm, EditProfileForm, EditProfileAdminForm,CommentForm,AskQuestionForm,AnswerForm
+# coding:utf-8
+
+from flask import render_template, redirect, request, url_for, flash, abort, current_app, make_response
+from flask_login import login_user, logout_user, login_required, current_user
+from ..models import User, Permission, Post, Role, Comment, Answer
+from .forms import PostForm, EditProfileForm, EditProfileAdminForm, CommentForm, AskQuestionForm, AnswerForm
 from .. import db, cache
 from . import main
 from ..decorators import admin_required, permission_required
 
-@main.route('/', methods=['GET','POST'])
-@cache.cached(timeout=300,key_prefix='index')#设置一个key_prefix来作为标记，然后，在内容更新的函数里面调用cache.delete('index')来删除缓存来保证用户访问到的内容是最新的
+import flask_whooshalchemyplus
+
+
+@main.route('/', methods=['GET', 'POST'])
+@cache.cached(timeout=5, key_prefix='index')
 def index():
-    print("index called")
-    # for i in (Post.query.outerjoin(Post.answers).order_by(Post.timestamp.desc()).order_by(Answer.agreements_num.asc()).all()):
-    #     for a in i.answers:
-    #         print(a.body)
-    #
-    # form = PostForm()
-    # page = request.args.get('page', 1, type=int)
-    # pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],error_out=False)
-    # posts = pagination.items
-    # return render_template('index.html', form=form, posts=posts,pagination=pagination)
+    """
+    设置一个key_prefix来作为标记，然后，在内容更新的函数里面调用cache.delete('index')来删除缓存来保证用户访问到的内容是最新的
 
-    # form = PostForm()
-    # if current_user.can(Permission.WRITE_ARTICLES) and \
-    #     form.validate_on_submit():
-    #     post = Post(body=form.body.data,author=current_user._get_current_object())
-    #     db.session.add(post)
-    #     return redirect(url_for('.index'))
-    # posts = Post.query.order_by(Post.timestamp.desc()).all()
-    # return render_template('index.html', form=form, posts=posts)
+    新文章对象的 author 属性值为表达式 current_user._get_current_object() 。变量current_user 由 Flask-Login 提供，
+    和所有上下文变量一样，也是通过线程内的代理对象实现。这个对象的表现类似用户对象，但实际上却是一个轻度包装，包含真正的用户对象。
+    数据库需要真正的用户对象，因此要调用 _get_current_object() 方法。
 
-    form = PostForm()
-    if current_user.can(Permission.WRITE_ARTICLES) and \
-            form.validate_on_submit():
-        #新文章对象的 author 属性值为表达式 current_user._get_current_object() 。变量current_user 由 Flask-Login 提供，和所有上下文变量一样，也是通过线程内的代理对象实现。这个对象的表现类似用户对象，但实际上却是一个轻度包装，包含真正的用户对象。数据库需要真正的用户对象，因此要调用 _get_current_object() 方法。
-        post = Post(body=form.body.data,author=current_user._get_current_object())
-        db.session.add(post)
-        return redirect(url_for('.index'))
-    #渲染的页数从请求的查询字符串（ request.args ）中获取，如果没有明确指定，则默认渲染第一页。参数 type=int 保证参数无法转换成整数时，返回默认值。
+    渲染的页数从请求的查询字符串（ request.args ）中获取如果没有明确指定则默认渲染第一页参数 type=int 保证参数无法转换成整数时，返回默认值。
+
+    为了显示某页中的记录，要把 all() 换成 Flask-SQLAlchemy 提供的 paginate() 方法。页数是 paginate() 方法的第一个参数
+    也是唯一必需的参数。可选参数 per_page 用来指定每页显示的记录数量；如果没有指定，则默认显示 20 个记录。另一个可选参数为 error_out
+    当其设为 True 时（默认值），如果请求的页数超出了范围，则会返回 404 错误；如果设为 False ，页数超出范围时会返回一个空列表。
+    为了能够很便利地配置每页显示的记录数量，参数 per_page 的值从程序的环境变量 FLASKY_POSTS_PER_PAGE 中读取。这样修改之后，
+    首页中的文章列表只会显示有限数量的文章。若想查看第 2 页中的文章，要在浏览器地址栏中的 URL 后加上查询字符串 ?page=2。
+    :return:
+    """
     page = request.args.get('page', 1, type=int)
     show_followed = False
+    query = Post.query
     if current_user.is_authenticated:
         show_followed = bool(request.cookies.get('show_followed', ''))
-    if show_followed:
-        query = current_user.followed_posts
-    else:
-        query = Post.query
-    #为了显示某页中的记录，要把 all() 换成 Flask-SQLAlchemy 提供的 paginate() 方法。页数是 paginate() 方法的第一个参数，也是唯一必需的参数。可选参数 per_page 用来指定每页显示的记录数量；如果没有指定，则默认显示 20 个记录。另一个可选参数为 error_out ，当其设为 True 时（默认值），如果请求的页数超出了范围，则会返回 404 错误；如果设为 False ，页数超出范围时会返回一个空列表。为了能够很便利地配置每页显示的记录数量，参数 per_page 的值从程序的环境变量 FLASKY_POSTS_PER_PAGE 中读取。这样修改之后，首页中的文章列表只会显示有限数量的文章。若想查看第 2 页中的文章，要在浏览器地址栏中的 URL 后加上查询字符串 ?page=2。
-    pagination = query.outerjoin(Post.answers).order_by(Post.timestamp.desc()).paginate(
-        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
-        error_out=False)
+        if show_followed:
+            query = current_user.followed_posts
+    pagination = query.outerjoin(Post.answers).order_by(Post.timestamp.desc()).paginate(page,
+                                                                                        per_page=current_app.config[
+                                                                                            'FLASKY_POSTS_PER_PAGE'],
+                                                                                        error_out=False)
     posts = pagination.items
-
-    import flask_whooshalchemyplus
     flask_whooshalchemyplus.index_one_model(Post)
-    return render_template('index4.html', form=form, posts=posts,show_followed=show_followed, pagination=pagination)
+    return render_template('index4.html', posts=posts, show_followed=show_followed, pagination=pagination)
 
-@main.route('/askquestion', methods=['GET','POST'])
+
+@main.route('/askquestion', methods=['GET', 'POST'])
 @login_required
 def askquestion():
-    # form = PostForm()
-    # page = request.args.get('page', 1, type=int)
-    # pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],error_out=False)
-    # posts = pagination.items
-    # return render_template('index.html', form=form, posts=posts,pagination=pagination)
+    """
+    # 新文章对象的 author 属性值为表达式 current_user._get_current_object() 。
+    # 变量current_user 由 Flask-Login 提供，和所有上下文变量一样，
+    # 也是通过线程内的代理对象实现。这个对象的表现类似用户对象，
+    # 但实际上却是一个轻度包装，包含真正的用户对象。数据库需要真正的用户对象，因此要调用 _get_current_object() 方法。
 
-    # form = PostForm()
-    # if current_user.can(Permission.WRITE_ARTICLES) and \
-    #     form.validate_on_submit():
-    #     post = Post(body=form.body.data,author=current_user._get_current_object())
-    #     db.session.add(post)
-    #     return redirect(url_for('.index'))
-    # posts = Post.query.order_by(Post.timestamp.desc()).all()
-    # return render_template('index.html', form=form, posts=posts)
-
+    # 渲染的页数从请求的查询字符串（ request.args ）中获取，如果没有明确指定，
+    # 则默认渲染第一页。参数 type=int 保证参数无法转换成整数时，返回默认值。
+    :return:
+    """
     form = AskQuestionForm()
-    if current_user.can(Permission.WRITE_ARTICLES) and \
-            form.validate_on_submit():
-        #新文章对象的 author 属性值为表达式 current_user._get_current_object() 。变量current_user 由 Flask-Login 提供，和所有上下文变量一样，也是通过线程内的代理对象实现。这个对象的表现类似用户对象，但实际上却是一个轻度包装，包含真正的用户对象。数据库需要真正的用户对象，因此要调用 _get_current_object() 方法。
-        post = Post(title=form.title.data,body=form.body.data,author=current_user._get_current_object())
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, body=form.body.data, author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    #渲染的页数从请求的查询字符串（ request.args ）中获取，如果没有明确指定，则默认渲染第一页。参数 type=int 保证参数无法转换成整数时，返回默认值。
+
     page = request.args.get('page', 1, type=int)
-    show_followed = False
+    query = Post.query
     if current_user.is_authenticated:
         show_followed = bool(request.cookies.get('show_followed', ''))
-    if show_followed:
-        query = current_user.followed_posts
-    else:
-        query = Post.query
-    #为了显示某页中的记录，要把 all() 换成 Flask-SQLAlchemy 提供的 paginate() 方法。页数是 paginate() 方法的第一个参数，也是唯一必需的参数。可选参数 per_page 用来指定每页显示的记录数量；如果没有指定，则默认显示 20 个记录。另一个可选参数为 error_out ，当其设为 True 时（默认值），如果请求的页数超出了范围，则会返回 404 错误；如果设为 False ，页数超出范围时会返回一个空列表。为了能够很便利地配置每页显示的记录数量，参数 per_page 的值从程序的环境变量 FLASKY_POSTS_PER_PAGE 中读取。这样修改之后，首页中的文章列表只会显示有限数量的文章。若想查看第 2 页中的文章，要在浏览器地址栏中的 URL 后加上查询字符串 ?page=2。
+        if show_followed:
+            query = current_user.followed_posts
+
+    # 为了显示某页中的记录，要把 all() 换成 Flask-SQLAlchemy 提供的 paginate() 方法。
+    # 页数是 paginate() 方法的第一个参数，也是唯一必需的参数。可选参数 per_page 用来指定每页显示的记录数量；
+    # 如果没有指定，则默认显示 20 个记录。另一个可选参数为 error_out ，当其设为 True 时（默认值），
+    # 如果请求的页数超出了范围，则会返回 404 错误；如果设为 False ，页数超出范围时会返回一个空列表。
+    # 为了能够很便利地配置每页显示的记录数量，参数 per_page 的值从程序的环境变量 FLASKY_POSTS_PER_PAGE 中读取。这
+    # 样修改之后，首页中的文章列表只会显示有限数量的文章。若想查看第 2 页中的文章，要在浏览器地址栏中的 URL 后加上查询字符串 ?page=2。
     pagination = query.order_by(Post.timestamp.desc()).paginate(
-        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
-        error_out=False)
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
     posts = pagination.items
 
     # import flask_whooshalchemyplus
     # flask_whooshalchemyplus.index_one_model(Post)
-    cache.delete('index')#删除缓存
+    cache.delete('index')  # 删除缓存
+    return render_template('askquestion.html', form=form, posts=posts, show_followed=show_followed,
+                           pagination=pagination)
 
-    return render_template('askquestion.html', form=form, posts=posts,show_followed=show_followed, pagination=pagination)
 
 @main.route('/user/<id>')
 def user(id):
     user = User.query.filter_by(id=id).first_or_404()
-    page = request.args.get('page',1,type=int)
-    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],error_out=False)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config[
+        'FLASKY_POSTS_PER_PAGE'], error_out=False)
     posts = pagination.items
-    return render_template('user.html', user=user, posts=posts,pagination=pagination)
+    return render_template('user.html', user=user, posts=posts, pagination=pagination)
+
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
@@ -126,6 +114,7 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
 
 @main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -154,15 +143,16 @@ def edit_profile_admin(id):
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
 
-#编辑文章的路由
+
+# 编辑文章的路由
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit(id):
     post = Post.query.get_or_404(id)
     if current_user != post.author and \
-        not current_user.can(Permission.ADMINISTER):
+            not current_user.can(Permission.ADMINISTER):
         abort(403)
-    form =PostForm()
+    form = PostForm()
     if form.validate_on_submit():
         post.body = form.body.data
         db.session.add(post)
@@ -171,24 +161,26 @@ def edit(id):
     form.body.data = post.body
     return render_template('edit_post.html', form=form)
 
-#删除文章的路由
+
+# 删除文章的路由
 @main.route('/delete/<int:id>', methods=['GET', 'POST'])
 @login_required
 def delete(id):
     post = Post.query.get_or_404(id)
     if current_user != post.author and \
-        not current_user.can(Permission.ADMINISTER):
+            not current_user.can(Permission.ADMINISTER):
         abort(403)
     db.session.delete(post)
     flash('问题已经删除')
     return redirect(url_for('.index'))
 
-#搜索文章的路由
+
+# 搜索文章的路由
 @main.route('/search/', methods=['GET', 'POST'])
 @login_required
 def search():
-    #获取搜索表单传过来的数据
-    keyword = request.form.get('q','default value')
+    # 获取搜索表单传过来的数据
+    keyword = request.form.get('q', 'default value')
 
     page = request.args.get('page', 1, type=int)
     show_followed = False
@@ -199,11 +191,11 @@ def search():
         error_out=False)
     posts = pagination.items
 
-    return render_template('searched.html',posts=posts, show_followed=show_followed, pagination=pagination)
+    return render_template('searched.html', posts=posts, show_followed=show_followed, pagination=pagination)
 
 
-#回答问题
-@main.route('/post/<int:id>',methods=['GET','POST'])
+# 回答问题
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
 @login_required
 def post(id):
     cache.delete('view//')
@@ -212,61 +204,63 @@ def post(id):
     form = AnswerForm()
     if form.validate_on_submit():
         answer = Answer(body=form.body.data,
-                          post=post,
-                          author=current_user._get_current_object()
-                          )
+                        post=post,
+                        author=current_user._get_current_object()
+                        )
         db.session.add(answer)
         flash('提交答案成功')
-        #评论按照时间戳顺序排列，新评论显示在列表的底部。提交评论后，请求结果是一个重定向，转回之前的 URL，但是在 url_for() 函数的参数中把 page 设为 -1 ，这是个特殊的页数，用来请求评论的最后一页，所以刚提交的评论才会出现在页面中。程序从查询字符串中获取页数，发现值为 -1 时，会计算评论的总量和总页数，得出真正要显示的页数。
+        # 评论按照时间戳顺序排列，新评论显示在列表的底部。提交评论后，请求结果是一个重定向，转回之前的 URL，但是在 url_for() 函数的参数中把 page 设为 -1 ，这是个特殊的页数，用来请求评论的最后一页，所以刚提交的评论才会出现在页面中。程序从查询字符串中获取页数，发现值为 -1 时，会计算评论的总量和总页数，得出真正要显示的页数。
         return redirect(url_for('.post', id=post.id, page=-1))
     page = request.args.get('page', 1, type=int)
     if page == -1:
-        page = (post.answers.count()-1)// \
+        page = (post.answers.count() - 1) // \
                current_app.config['FLASKY_ANSWERS_PER_PAGE'] + 1
     pagination = post.answers.order_by(Answer.agreements_num.desc()).paginate(
-            page,per_page=current_app.config['FLASKY_ANSWERS_PER_PAGE'],error_out=False
-        )
+        page, per_page=current_app.config['FLASKY_ANSWERS_PER_PAGE'], error_out=False
+    )
     answers = pagination.items
 
-    return render_template('post.html', posts=[post], form=form, answers=answers,pagination=pagination)
+    return render_template('post.html', posts=[post], form=form, answers=answers, pagination=pagination)
 
-#对答案进行评价
-@main.route('/comment/<int:id>',methods=['GET','POST'])
+
+# 对答案进行评价
+@main.route('/comment/<int:id>', methods=['GET', 'POST'])
 @login_required
 def comment(id):
     answer = Answer.query.get_or_404(id)
     form = CommentForm()
     if form.validate_on_submit():
-        comment = Comment(body=form.body.data,answer=answer,author=current_user._get_current_object()
+        comment = Comment(body=form.body.data, answer=answer, author=current_user._get_current_object()
                           )
         db.session.add(comment)
         flash('评论成功')
         return redirect(url_for('main.comment', id=answer.id, page=-1))
     page = request.args.get('page', 1, type=int)
     if page == -1:
-        page = (answer.comments.count()-1)// \
+        page = (answer.comments.count() - 1) // \
                current_app.config['FLASKY_ANSWERS_PER_PAGE'] + 1
     pagination = answer.comments.order_by(Comment.timestamp.asc()).paginate(
-            page,per_page=current_app.config['FLASKY_ANSWERS_PER_PAGE'],error_out=False
-        )
+        page, per_page=current_app.config['FLASKY_ANSWERS_PER_PAGE'], error_out=False
+    )
     comments = pagination.items
 
-    return render_template('comment.html', answers=[answer], form=form, comments=comments,pagination=pagination)
+    return render_template('comment.html', answers=[answer], form=form, comments=comments, pagination=pagination)
+
 
 @main.route('/moderate')
 @login_required
 @permission_required(Permission.MODERATE_ANSWERS)
 def moderate():
-    page = request.args.get('page',1,type=int)
+    page = request.args.get('page', 1, type=int)
     pagination = Answer.query.order_by(Answer.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_ANSWERS_PER_PAGE'],
         error_out=False
     )
     answers = pagination.items
-    return render_template('moderate.html', answers=answers,pagination=pagination,page=page)
+    return render_template('moderate.html', answers=answers, pagination=pagination, page=page)
 
 
-#“关注”路由和视图函数
+# “关注”路由和视图函数
 @main.route('/follow/<id>')
 @login_required
 @permission_required(Permission.FOLLOW)
@@ -282,7 +276,8 @@ def follow(id):
     flash('You are now following %s.' % id)
     return redirect(url_for('.user', id=id))
 
-#取消关注
+
+# 取消关注
 @main.route('/unfollow/<id>')
 @login_required
 @permission_required(Permission.FOLLOW)
@@ -298,7 +293,8 @@ def unfollow(id):
     flash('You are not following %s anymore.' % id)
     return redirect(url_for('.user', id=id))
 
-#“关注者”路由和视图函数
+
+# “关注者”路由和视图函数
 @main.route('/followers/<username>')
 @login_required
 def followers(username):
@@ -315,6 +311,7 @@ def followers(username):
     return render_template('followers.html', user=user, title="Followers of",
                            endpoint='.followers', pagination=pagination,
                            follows=follows)
+
 
 @main.route('/followed-by/<username>')
 @login_required
@@ -333,7 +330,8 @@ def followed_by(username):
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
 
-#点赞
+
+# 点赞
 @main.route('/agree/<id>')
 @login_required
 def agree(id):
@@ -348,7 +346,8 @@ def agree(id):
     flash('点赞成功')
     return redirect(url_for('.index'))
 
-#取消点赞
+
+# 取消点赞
 @main.route('/unagree/<id>')
 @login_required
 def unagree(id):
@@ -364,9 +363,7 @@ def unagree(id):
     return redirect(url_for('.index', id=id))
 
 
-
-
-#指向这两个路由的链接添加在首页模板中。点击这两个链接后会为 show_followed cookie 设定适当的值，然后重定向到首页。cookie 只能在响应对象中设置，因此这两个路由不能依赖 Flask，要使用 make_response()方法创建响应对象。
+# 指向这两个路由的链接添加在首页模板中。点击这两个链接后会为 show_followed cookie 设定适当的值，然后重定向到首页。cookie 只能在响应对象中设置，因此这两个路由不能依赖 Flask，要使用 make_response()方法创建响应对象。
 @main.route('/all')
 @login_required
 def show_all():
@@ -381,6 +378,7 @@ def show_followed():
     resp = make_response(redirect(url_for('.index')))
     resp.set_cookie('show_followed', '1')
     return resp
+
 
 @main.route('/moderate/enable/<int:id>')
 @login_required
